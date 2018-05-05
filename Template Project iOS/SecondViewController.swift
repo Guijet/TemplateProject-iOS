d546c91d40b14e1df26e86eb0c242d9a97b849c4
Unknown <Benoit.Charbonneau2@USherbrooke.ca>
2018-05-05 15:03:40 -0400

    Merge branch 'ben' into dev

--- Template Project iOS/SecondViewController.swift
+++ Template Project iOS/SecondViewController.swift
@@ -9,30 +9,9 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    var receivedText:String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Mon titre"
-        setUpLabel()
-        getServerItemsTest()
-    }
-    
-    func setUpLabel(){
-        let lblTest = UILabel()
-        lblTest.createLabel(x: rw(100), y: rw(200), width: rw(200), height: rh(50), textColor: UIColor.red, fontName: "Lato-Regular", fontSize: rw(15), textAignment: .center, text: receivedText)
-        self.view.addSubview(lblTest)
-    }
-    
-    
-    func getServerItemsTest(){
-        let url = "https://api.influenceme-app.com/influencer/offers"
-        let response = Utility().getJson(url: url, method: "GET", needToken:true)
-        if(!response.getIsError()){
-            let data = response.getDict()
-        }
-        
-        
+        self.navigationController?.navigationBar.isHidden = false
     }
 }
